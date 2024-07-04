7a7f4e0fc0466a2f4e21ae85d3d913707ae433b7
minhttran <trcongminh.261002@gmail.com>
2024-07-04 16:14:10 +0700

    Authentication and animation landing page section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
+import { Reviews } from "@/components/Reviews";
 import { Check, Star } from "lucide-react";
+
 
 
 export default function Home() {
@@ -81,6 +84,85 @@ export default function Home() {
           </div>
 
         </MaxWidthWrapper>
+      </section>
+
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tighter text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              What our <span className="relative px-2">customers
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+              </span> say
+            </h2>
+
+            <img src="/snake-2.png" className="order-0 lg:order-2 w-24" />
+          </div>
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-500 fill-green-600" />
+                <Star className="h-5 w-5 text-green-500 fill-green-600" />
+                <Star className="h-5 w-5 text-green-500 fill-green-600" />
+                <Star className="h-5 w-5 text-green-500 fill-green-600" />
+                <Star className="h-5 w-5 text-green-500 fill-green-600" />
+              </div>
+
+              <div className="text-lg leading-8">
+                <p>"The case feels durable and I event got a compliment on the design.
+                  Had the case for two and a half months now and
+                  <span className="p-0.5 bg-slate-800 text-white"> the image is super clear </span>, on the case I had before, the image started fading
+                  into yellow-ish color after a couple week. Love it!"
+                </p>
+              </div>
+
+              <div className="flex gap-4 mt-2">
+                <img className="rounded-full w-12 h-12 object-cover" src="/users/user-1.png" alt="user image" />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Thanh Huyen</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="w-4 h-4 stroke-[3px] text-green-600" />
+                    <div className="text-sm">Verified Purchase</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-500 fill-green-600" />
+                <Star className="h-5 w-5 text-green-500 fill-green-600" />
+                <Star className="h-5 w-5 text-green-500 fill-green-600" />
+                <Star className="h-5 w-5 text-green-500 fill-green-600" />
+                <Star className="h-5 w-5 text-green-500 fill-green-600" />
+              </div>
+
+              <div className="text-lg leading-8">
+                <p>"I usually keep my phone together with my keys in pocket and that led to
+                  some pretty heavy scratchmarks on all of my last phone case. This one, besides
+                  a barely noticeable scratch on the corner, {' '}
+                  <span className="p-0.5 bg-slate-800 text-white"> looks brand new after about half a year </span>
+                  , I dig it."
+                </p>
+              </div>
+
+              <div className="flex gap-4 mt-2">
+                <img className="rounded-full w-12 h-12 object-cover" src="/users/user-4.jpg" alt="user image" />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Khanh Van</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="w-4 h-4 stroke-[3px] text-green-600" />
+                    <div className="text-sm">Verified Purchase</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+
+        <div className="pt-16">
+          <Reviews />
+        </div>
       </section>
     </div>
   );
--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -11,7 +11,7 @@ const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
         <div className={
             cn('relative pointer-events-none z-50 overflow-hidden', className)} {...props}>
 
-            <img src={dark ? '/phone-template-dark-edges.png' : 'phone-template-white-edges.png'} alt="phone image" className="pointer-events-none z-50 select-none" />
+            <img src={dark ? '/phone-template-dark-edges.png' : '/phone-template-white-edges.png'} alt="phone image" className="pointer-events-none z-50 select-none" />
 
             <div className="absolute -z-10 inset-0">
                 <img src={imgSrc} alt="pverlaying phone image" className="object-cover" />
